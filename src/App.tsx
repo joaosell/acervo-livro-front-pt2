4c867da80a5db7dff427b175cff8882112b45db7
Joao Sell <jpjpsell@gmail.com>
2026-04-04 20:41:25 -0300

    testing request w/ back

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import { Button, Table, Typography } from "antd";
+import { useState } from "react";
 
 function App() {
-  return <>HELLO WORLD</>;
+  const [editoras, setEditoras] = useState([]);
+
+  const handleClick = async () => {
+    const response = await fetch("http://localhost:3000/editoras");
+    const data = await response.json();
+    setEditoras(data);
+  };
+  const columns = [
+    {
+      title: "Nome",
+      dataIndex: "nome",
+      key: "nome",
+    },
+    {
+      title: "Cidade",
+      dataIndex: "cidade",
+      key: "cidade",
+    },
+  ];
+
+  return (
+    <>
+      <Typography.Title>Acervo de livros</Typography.Title>
+      <Typography>Clique aqui para mostrar todas as editoras</Typography>
+      <Button onClick={handleClick}>mostrar</Button>
+      <Table dataSource={editoras} columns={columns} />
+    </>
+  );
 }
 
 export default App;
