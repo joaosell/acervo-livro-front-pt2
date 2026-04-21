748942da6dd1d4e80105fc5bec7249204bdf0dcd
Joao Sell <jpjpsell@gmail.com>
2026-04-21 11:27:24 -0300

    fixing descricao update in categorias

--- src/services/categoriaService.tsx
+++ src/services/categoriaService.tsx
@@ -4,7 +4,7 @@ export const categoriaService = {
   getAll: () => api.get("/categorias"),
   create: (body: { nome: string; descricao: string }) =>
     api.post("/categorias", body),
-  update: (id: number, body: { nome: string }) =>
+  update: (id: number, body: { nome: string; descricao: string }) =>
     api.patch(`/categorias/${id}`, body),
   remove: (id: number) => api.delete(`/categorias/${id}`),
 };
